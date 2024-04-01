c122979c5aee1f7640444aed46abfd0b67c730fe
Rafaela Borba Menezes <rafaelaborbamenezes@gmail.com>
2024-04-01 10:00:54 -0300

    Adição dos atributos da classe Veiculos.cs

---
+++ Veiculo.cs
@@ -0,0 +1,11 @@
+public class Veiculo{ 
+    public string Modelo {get; set;}
+    public double Preco {get; set;} 
+    public int QuantidadeEstoque {get; set;}  
+    public int Ano {get; set;}
+    public string Marca {get; set;} 
+    public string Direcao {get; set;}
+
+      
+
+}
