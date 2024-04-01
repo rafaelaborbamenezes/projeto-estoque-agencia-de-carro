21cb1c10e3e46fe16bd515a18f3865e5c328d941
Rafaela Borba Menezes <rafaelaborbamenezes@gmail.com>
2024-04-01 11:37:22 -0300

    Criação da função Entrada na classe Agencia

--- Agencia.cs
+++ Agencia.cs
@@ -10,8 +10,7 @@ class Agencia{
                 Veiculo item = veiculos[pos]; 
                 novoVetor[pos] = item;      
               } 
-               novoVetor[novoVetor.Length-1] = veiculo; 
-                         
+                novoVetor[novoVetor.Length-1] = veiculo; 
                          
                 veiculos = novoVetor; 
         } 
@@ -22,34 +21,39 @@ class Agencia{
               
               Console.WriteLine($"{cont}. {item.Modelo} (R$ {item.Preco}) - {item.QuantidadeEstoque} no estoque"); 
               cont++; 
-             } 
+              } 
         }
    
         public void Remover(int posicao){  
            
-        if (posicao >= 1 && posicao <= veiculos.Length)
-        {
-        Veiculo[] novoVetor = new Veiculo[veiculos.Length - 1];
-
-        for (int i = 0, j = 0; i < veiculos.Length; i++)
-        {
-            if (i != posicao - 1)
+            if (posicao >= 1 && posicao <= veiculos.Length)
             {
+                Veiculo[] novoVetor = new Veiculo[veiculos.Length - 1];
+
+                for (int i = 0, j = 0; i < veiculos.Length; i++)
+                {
+                if (i != posicao - 1)
+                {
                 novoVetor[j++] = veiculos[i];
+                }
             }
-        }
+                veiculos = novoVetor;
 
-        veiculos = novoVetor;
-
-        Console.WriteLine("Veículo de posição " + posicao + " removido.");
-        }
-        else
-        {
-        Console.WriteLine("Posição inválida. Certifique-se de fornecer uma posição válida no intervalo do vetor.");
-        }
+                Console.WriteLine("Veículo de posição " + posicao + " removido.");
+            }
+            else
+            {
+                Console.WriteLine("Posição inválida. Certifique-se de fornecer uma posição válida no intervalo do vetor.");
+            }
      
         }  
+     
+        public void Entrada(int pos, int quant){ 
 
+           Veiculo veiculo = veiculos[pos-1]; 
+           veiculo.QuantidadeEstoque += quant;
+
+        }
          
           
            
@@ -57,5 +61,5 @@ class Agencia{
              
               
                
-               
+
     }
