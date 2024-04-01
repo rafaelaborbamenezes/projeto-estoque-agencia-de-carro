27abcd01679c53f4d5ac252dd17ff7ef9e368f79
Rafaela Borba Menezes <rafaelaborbamenezes@gmail.com>
2024-04-01 10:32:02 -0300

    Implantação do menu principal

--- Program.cs
+++ Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
-}
+         
+      int menu =0; 
+            
+        Console.WriteLine("\n\nO que você deseja fazer?");
+        Console.WriteLine("1. NOVO");
+        Console.WriteLine("2. LISTAR PRODUTOS");
+        Console.WriteLine("3. REMOVER PRODUTOS");
+        Console.WriteLine("4. ENTRADA ESTOQUE"); 
+        Console.WriteLine("5. SAÍDA ESTOQUE"); 
+        Console.WriteLine("0. SAIR");
+        menu = Convert.ToInt32(Console.ReadLine());    
+           
+            
+        }
+              
+        }      
+
+    
