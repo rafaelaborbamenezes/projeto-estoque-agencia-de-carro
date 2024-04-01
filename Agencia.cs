9f66529c2dea5672fa0a2b0250ea266ef4a51625
Rafaela Borba Menezes <rafaelaborbamenezes@gmail.com>
2024-04-01 10:57:41 -0300

    Criação da função Cadastrar na classe Agencia

--- Agencia.cs
+++ Agencia.cs
@@ -1,7 +1,17 @@
 class Agencia{ 
      
       
-       
+       Veiculo[] veiculos = new Veiculo[0];
         
-         
-}
+        public void Cadastrar(Veiculo veiculo){ 
+            Veiculo[] novoVetor = new Veiculo[veiculos.Length +1]; 
+            for (int pos = 0; pos < veiculos.Length; pos++)  
+              {
+                Veiculo item = veiculos[pos]; 
+                novoVetor[pos] = item;      
+              } 
+               novoVetor[novoVetor.Length-1] = veiculo; 
+                         
+                         
+                veiculos = novoVetor; 
+}}
