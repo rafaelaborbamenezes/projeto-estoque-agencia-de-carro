cca2d67ed95429b1f654da127ea9c5a47a427ab1
Rafaela Borba Menezes <rafaelaborbamenezes@gmail.com>
2024-04-01 11:26:00 -0300

    Criação da função Remover na classe Agencia

--- Agencia.cs
+++ Agencia.cs
@@ -25,7 +25,37 @@ class Agencia{
              } 
         }
    
-    
+        public void Remover(int posicao){  
+           
+        if (posicao >= 1 && posicao <= veiculos.Length)
+        {
+        Veiculo[] novoVetor = new Veiculo[veiculos.Length - 1];
+
+        for (int i = 0, j = 0; i < veiculos.Length; i++)
+        {
+            if (i != posicao - 1)
+            {
+                novoVetor[j++] = veiculos[i];
+            }
+        }
+
+        veiculos = novoVetor;
+
+        Console.WriteLine("Veículo de posição " + posicao + " removido.");
+        }
+        else
+        {
+        Console.WriteLine("Posição inválida. Certifique-se de fornecer uma posição válida no intervalo do vetor.");
+        }
      
-      
-      }
+        }  
+
+         
+          
+           
+            
+             
+              
+               
+               
+    }
